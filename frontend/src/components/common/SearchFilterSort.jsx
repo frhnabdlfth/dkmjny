ea64c9b2f6987e58d3f6bd72ecb6d3d8235aad8f
frhnabdlfth <frhnabdlfth270901@gmail.com>
2026-06-08 01:10:05 +0700

    Update SearchFilterSort.jsx

--- frontend/src/components/common/SearchFilterSort.jsx
+++ frontend/src/components/common/SearchFilterSort.jsx
@@ -25,51 +25,51 @@ export default function SearchFilterSort({
       </div>
 
       {/* Sort */}
-      {sortOptions.length > 0 && (
-        <div className="relative shrink-0">
-          <ArrowUpDown
-            size={16}
-            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none"
-          />
-          <select
-            value={sortValue}
-            onChange={(e) => onSortChange(e.target.value)}
-            className="h-11 w-11 sm:w-auto pl-9 sm:pr-8 rounded-xl border border-gray-300 bg-gray-50 appearance-none cursor-pointer text-transparent sm:text-gray-800"
-          >
-            {sortOptions.map((option) => (
-              <option key={option.value} value={option.value} className="text-gray-800">
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </div>
-      )}
+{sortOptions.length > 0 && (
+  <div className="relative shrink-0">
+    <ArrowUpDown
+      size={16}
+      className="absolute left-1/2 -translate-x-1/2 sm:left-3 sm:translate-x-0 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none"
+    />
+    <select
+      value={sortValue}
+      onChange={(e) => onSortChange(e.target.value)}
+      className="h-11 w-11 sm:w-auto pl-9 sm:pr-8 rounded-xl border border-gray-300 bg-gray-50 appearance-none cursor-pointer text-transparent sm:text-gray-800"
+    >
+      {sortOptions.map((option) => (
+        <option key={option.value} value={option.value} className="text-gray-800">
+          {option.label}
+        </option>
+      ))}
+    </select>
+  </div>
+)}
 
-      {/* Filter */}
-      {filterOptions.length > 0 && (
-        <div className="relative shrink-0">
-          <Funnel
-            size={16}
-            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none"
-          />
-          <select
-            value={filterValue}
-            onChange={(e) => onFilterChange(e.target.value)}
-            className="h-11 w-11 sm:w-auto pl-9 sm:pr-8 rounded-xl border border-gray-300 bg-gray-50 appearance-none cursor-pointer text-transparent sm:text-gray-800"
-          >
-            <option value="" className="text-gray-800">Semua Data</option>
-            {filterOptions.map((group) => (
-              <optgroup key={group.label} label={group.label}>
-                {group.options.map((option) => (
-                  <option key={option.value} value={option.value} className="text-gray-800">
-                    {option.label}
-                  </option>
-                ))}
-              </optgroup>
-            ))}
-          </select>
-        </div>
-      )}
+{/* Filter */}
+{filterOptions.length > 0 && (
+  <div className="relative shrink-0">
+    <Funnel
+      size={16}
+      className="absolute left-1/2 -translate-x-1/2 sm:left-3 sm:translate-x-0 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none"
+    />
+    <select
+      value={filterValue}
+      onChange={(e) => onFilterChange(e.target.value)}
+      className="h-11 w-11 sm:w-auto pl-9 sm:pr-8 rounded-xl border border-gray-300 bg-gray-50 appearance-none cursor-pointer text-transparent sm:text-gray-800"
+    >
+      <option value="" className="text-gray-800">Semua Data</option>
+      {filterOptions.map((group) => (
+        <optgroup key={group.label} label={group.label}>
+          {group.options.map((option) => (
+            <option key={option.value} value={option.value} className="text-gray-800">
+              {option.label}
+            </option>
+          ))}
+        </optgroup>
+      ))}
+    </select>
+  </div>
+)}
     </div>
   );
 }
